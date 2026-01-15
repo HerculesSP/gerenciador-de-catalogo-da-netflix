7621629130a358ad8616047eb793028248ca92c6
Hércules <herculespereira99@gmail.com>
2026-01-15 20:19:23 -0300

    Função 'fnGetAllMovies' finalizada

---
+++ fnGetAllMovies/fnGetAllMovies/Function1.cs
@@ -0,0 +1,45 @@
+using fnGetMovieDetail;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace fnGetAllMovies;
+
+public class Function1
+{
+    private readonly ILogger<Function1> _logger;
+    private readonly CosmosClient _cosmosClient;
+    public Function1(ILogger<Function1> logger, CosmosClient cosmosClient)
+    {
+        _logger = logger;
+        _cosmosClient = cosmosClient;
+    }
+
+    [Function("all")]
+    public async Task<HttpResponseData> RunAsync([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequestData req)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
+        var container = _cosmosClient.GetContainer("Netflix", "movies");
+        var id = req.Query["id"];
+        var query = $"SELECT * FROM c";
+        var queryDefinition = new QueryDefinition(query);
+        var result = container.GetItemQueryIterator<MovieResult>(queryDefinition);
+        var results = new List<MovieResult>();
+
+        while (result.HasMoreResults)
+        {
+            foreach (var item in await result.ReadNextAsync())
+            {
+                results.Add(item);
+            }
+        }
+        var res = req.CreateResponse(System.Net.HttpStatusCode.OK);
+        await res.WriteAsJsonAsync(results);
+
+        return res;
+
+    }
+}
