c069d9df5ca3bcc535af8bbb29d7bf9c2507ae60
Hércules <herculespereira99@gmail.com>
2026-01-15 17:13:56 -0300

    Função 'fnPostDataStorage' finalizada

---
+++ fnPostDataStorage/fnPostDataStorage/Program.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = FunctionsApplication.CreateBuilder(args);
+
+builder.ConfigureFunctionsWebApplication();
+
+builder.Services.Configure<KestrelServerOptions>(options =>
+{
+    options.Limits.MaxRequestBodySize = 1024 * 1024 * 100;
+});
+
+builder.Build().Run();
